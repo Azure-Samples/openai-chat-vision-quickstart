4a6f9d4ca10fcc12a9634df832fc50691f639e34
Zhijie Huang <zhihuan@microsoft.com>
2024-09-02 06:20:13 +0000

    replace az cli with python sdk

--- scripts/pre-down.sh
+++ scripts/pre-down.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-az cognitiveservices account deployment delete --name $AZURE_OPENAI_RESOURCE_NAME --resource-group $AZURE_RESOURCE_GROUP --deployment-name $AZURE_OPENAI_CHATGPT_DEPLOYMENT
+# Get the directory of the current script
+script_dir=$(dirname "$0")
+
+# Load environment variables from azd env
+subscription_id=$(azd env get-value AZURE_SUBSCRIPTION_ID)
+resource_name=$(azd env get-value AZURE_OPENAI_RESOURCE_NAME)
+resource_group=$(azd env get-value AZURE_RESOURCE_GROUP)
+deployment_name=$(azd env get-value AZURE_OPENAI_CHATGPT_DEPLOYMENT)
+
+# Run the Python script with the retrieved values
+python "$script_dir/pre-down.py" --subscription-id $subscription_id --resource-name $resource_name --resource-group $resource_group --deployment-name $deployment_name
