1e2d903c3c0ea5bb92344bb491e755169b867889
Pamela Fox <pamela.fox@gmail.com>
2024-10-23 13:20:23 -0700

    New line location

--- scripts/write_env.ps1
+++ scripts/write_env.ps1
@@ -10,8 +10,8 @@ $azureOpenAiDeployment = azd env get-value AZURE_OPENAI_DEPLOYMENT
 $azureOpenAiApiVersion = azd env get-value AZURE_OPENAI_API_VERSION
 
 Add-Content -Path $envFilePath -Value "OPENAI_HOST=azure"
-Add-Content -Path $envFilePath -Value ""
 Add-Content -Path $envFilePath -Value "OPENAI_MODEL=$azureOpenAiDeployment"
+Add-Content -Path $envFilePath -Value ""
 Add-Content -Path $envFilePath -Value "AZURE_OPENAI_ENDPOINT=$azureOpenAiEndpoint"
 Add-Content -Path $envFilePath -Value "AZURE_OPENAI_API_VERSION=$azureOpenAiApiVersion"
 Add-Content -Path $envFilePath -Value ""
