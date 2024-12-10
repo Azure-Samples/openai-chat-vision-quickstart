0f7b2b4618db67dadfc8e452c8af4b5bb4b80815
Pamela Fox <pamela.fox@gmail.com>
2024-12-10 11:01:01 -0800

    Dont use shadow DOM, add cute animation like VSCode

--- src/quartapp/static/speech.js
+++ src/quartapp/static/speech.js
@@ -1,43 +1,42 @@
 class SpeechRecordButton extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-                integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
-                integrity="sha256-4RctOgogjPAdwGbwq+rxfwAmSpZhWaafcZR9btzUk18=" crossorigin="anonymous">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.2.3/dist/cosmo/bootstrap.min.css"
-                integrity="sha256-axRDISYf7Hht1KhcMnfDV2nq7hD/8Q9Rxa0YlW/o3NU=" crossorigin="anonymous">
-            <button class="btn btn-secondary" type="button" id="record-button">
+        this.innerHTML = `
+            <button class="btn btn-outline-secondary" type="button">
                 <i class="bi bi-mic"></i>
-            </button>
-        `;
+            </button>`;
         this.isRecording = false;
         this.speechRecognition = this.useCustomSpeechRecognition();
-        this.recordButton = this.shadowRoot.getElementById('record-button');
+        this.recordButton = this.querySelector('button');
         this.recordButton.addEventListener('click', () => this.toggleRecording());
+    }
+
+    renderButtonOn() {
+        this.recordButton.classList.add('speech-active');
+        this.recordButton.innerHTML = '<i class="bi bi-mic-fill"></i>';
+    }
+
+    renderButtonOff() {
+        this.recordButton.classList.remove('speech-active');
+        this.recordButton.innerHTML = '<i class="bi bi-mic"></i>';
     }
 
     useCustomSpeechRecognition() {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
         if (!SpeechRecognition) {
-            console.error("SpeechRecognition not supported");
+            this.dispatchEvent(new CustomEvent('speecherror', { detail: { error: "SpeechRecognition not supported" } }));
             return null;
         }
         const speechRecognition = new SpeechRecognition();
         speechRecognition.lang = navigator.language || navigator.userLanguage;
-        console.log(`Using language: ${speechRecognition.lang}`);
         speechRecognition.interimResults = false;
         speechRecognition.maxAlternatives = 1;
         return speechRecognition;
     }
 
     startRecording() {
-        console.log("Starting speech recognition");
         if (this.speechRecognition == null) {
-            console.error("SpeechRecognition not supported");
-            return;
+            this.dispatchEvent(new CustomEvent('speecherror', { detail: { error: "SpeechRecognition not supported" } }));
         }
 
         this.speechRecognition.onresult = (event) => {
@@ -45,15 +44,13 @@ class SpeechRecordButton extends HTMLElement {
             for (const result of event.results) {
                 input += result[0].transcript;
             }
-            console.log(`Speech recognition result: ${input}`);
             this.dispatchEvent(new CustomEvent('speechresult', { detail: { transcript: input } }));
         };
 
         this.speechRecognition.onend = () => {
-            console.log("Speech recognition ended");
             // NOTE: In some browsers (e.g. Chrome), the recording will stop automatically after a few seconds of silence.
             this.isRecording = false;
-            this.recordButton.innerHTML = '<i class="bi bi-mic"></i>';
+            this.renderButtonOff();
             this.dispatchEvent(new Event('speechend'));
         };
 
@@ -72,7 +69,7 @@ class SpeechRecordButton extends HTMLElement {
 
         this.speechRecognition.start();
         this.isRecording = true;
-        this.recordButton.innerHTML = '<i class="bi bi-mic-fill"></i>';
+        this.renderButtonOn();
     }
 
     toggleRecording() {
